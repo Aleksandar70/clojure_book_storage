c1ee524daaf9febfc57bc04a5f5afbca7d5d6aac
Aleksandar Berdovic <aleksandar.berdovic@gmail.com>
2019-07-27 19:24:04 +0200

    added dependencies

--- project.clj
+++ project.clj
@@ -3,5 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns book-storage.core})
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [enlive "1.1.6"]
+                 [ring "1.7.1"]
+                 [compojure "1.6.1"]
+                 [incanter "1.5.6"]
+                 [com.cemerick/friend "0.2.3"]]
+  :repl-options {:init-ns book-storage.core}
+  :main book_storage.core)
