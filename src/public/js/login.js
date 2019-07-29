c18f6993862113f916cf7218dd611d738a6a50fc
Aleksandar Berdovic <aleksandar.berdovic@gmail.com>
2019-07-29 21:08:54 +0200

    added login functionality

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,17 +1,40 @@
-$('.toggle-register').click(function () {
-  $(this).addClass('active');
-  $('.toggle-login').removeClass('active');
-  $('.login-body').slideUp("fast");
-  $('.register-body').delay(275).slideDown("fast");
-});
+(function ($) {
+	"use strict";
 
-$('.toggle-login').click(function () {
-  $(this).addClass('active');
-  $('.toggle-register').removeClass('active');
-  $('.register-body').slideUp("fast");
-  $('.login-body').delay(275).slideDown("fast");
-});
+	var options = {
+		'btn-loading': '<i class="fa fa-spinner fa-pulse"></i>',
+		'btn-success': '<i class="fa fa-check"></i>',
+		'btn-error': '<i class="fa fa-remove"></i>',
+		'msg-success': 'All Good! Redirecting...',
+		'msg-error': 'Wrong login credentials!',
+		'useAJAX': true,
+	};
 
-$('#registered').click(function () {
-  $('.toggle-login').click();
-});
+	$("#login").validate({
+		rules: {
+			username: "required",
+			password: "required",
+		},
+		errorClass: "form-invalid"
+	});
+
+	$("#login").submit(function () {
+		if (options['useAJAX'] == true) {
+
+			dummy_submit_form($(this));
+
+			return false;
+		}
+	});
+
+	function dummy_submit_form($form) {
+		if ($form.valid()) {
+			form_loading($form);
+
+			setTimeout(function () {
+				form_success($form);
+			}, 2000);
+		}
+	}
+
+})(jQuery);
